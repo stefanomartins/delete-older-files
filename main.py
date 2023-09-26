7a7bc0c3a311f61d2ff9d60c5b094dbf209db069
Stefano Martins <cin_scruz@uolinc.com>
2023-09-26 10:36:17 -0300

    Implementando argparser

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import datetime
 import os
-import sys
-
-base_dir = sys.argv[1]
-n = int(sys.argv[2])
+import argparse
 
 
-for (root, dirs, files) in os.walk(base_dir, topdown=True):
+parser = argparse.ArgumentParser()
+parser.add_argument("base_dir", type=str)
+parser.add_argument("-n", action="store", type=int, required=True)
+parser.add_argument("--dry-run", action="store_true")
+
+args = parser.parse_args()
+
+base_dir = args.base_dir
+n = args.n
+dry_run = args.dry_run
+
+
+for root, dirs, files in os.walk(base_dir, topdown=True):
     for file in files:
         file_path = os.path.join(root, file)
 
@@ -18,6 +27,9 @@ for (root, dirs, files) in os.walk(base_dir, topdown=True):
         n_days = (datetime.datetime.now() - date_modified_dt).days
 
         if n_days > n:
-            print(f"Apagando arquivo {file_path}")
-            # os.remove(file_path) 
 
+            if dry_run:
+                print("Modo dry run ativado")
+            else:
+                print(f"Apagando arquivo {file_path}")
+                os.remove(file_path)
